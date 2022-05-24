913503a7c8ec5d387f63e68a75f0a8313621621d
Draden Liang Han Sheng <hanshengliang@outlook.com>
2022-05-24 10:27:36 +0800

    :rocket: Firebase Hosting

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import Map, { Marker, Popup, Source, Layer } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+
 /*
  *Map Box Access TOKEN
  */
